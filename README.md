27f7edf996576ccb018f0360d00486e78d7c661d
BaptistPos <baptist.possenier@student.kuleuven.be>
2026-03-02 16:44:24 +0100

    JSON-data plaatsen op console met testopstelling HUIS

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # IB3 Dust experiment - DIEPTE
 
 In dit project ga ik experimenteren hoe het signaal van de DUST-sensor wordt aangepast, naargelang hoe die de sensor onder de grond zit.
+
+### Testopstelling - HUIS
+Nog niet getest als de sensor in een medium zit (onder de grond)
+
+#### Gateways:
+- ttn-vives-indoor-06: keuken
+- ttn-vives-indoor-11: lamp
+- ttn-vives-indoor-07: kast
+- ttn-vives-indoor-05: computer
+- ttn-vives-indoor-13: boven
+
+#### Sensoren:
+- 2211-001-0003: computer
+- 2303-002-0010: trap
+- 2407-003-0078: midden
--- src/read_json.py
+++ src/read_json.py
@@ -1,6 +1,17 @@
 import json
 
-with open('data/data_huis.json', 'r') as file:
-    data = json.load(file)
+with open('data/data_huis_trap.json', 'r') as file:
+    raw_data = json.load(file)
 
-print(json.dumps(data, indent=4))
+for i in range(len(raw_data) - 1):
+    if (raw_data[i]["name"] == "as.up.data.forward"):
+        print(f"Sensor ({json.dumps(raw_data[i]["data"]["uplink_message"]["f_cnt"])}, {json.dumps(raw_data[i]["data"]["uplink_message"]["consumed_airtime"])}): {json.dumps(raw_data[i]["data"]["end_device_ids"]["device_id"])})")
+        for j in range(len(raw_data[i]["data"]["uplink_message"]["rx_metadata"])):
+            print("\tGateway: " + json.dumps(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["gateway_ids"]["gateway_id"]))
+            print("\t\tRSSI: " + json.dumps(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["rssi"]))
+            print("\t\tSNR: " + json.dumps(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["snr"]))
+            print("\t\tReceived: " + json.dumps(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["received_at"]))
+        print("\n")
+
+
+
