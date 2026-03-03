1f87c021eea9566cf37803326c07a9560e2749ea
BaptistPos <baptist.possenier@student.kuleuven.be>
2026-03-03 11:36:56 +0100

    Update panda framework door 2 frameworks (SENSOR & DATA)

--- src/get_json.py
+++ src/get_json.py
@@ -2,28 +2,44 @@ import json
 import pandas as pd
 
 PATH = "data/data_garage.json"
-id = []
-gateway = []
-sensor = []
-rssi = []
-snr = []
 
+data_id = []
+data_gateway = []
+data_rssi = []
+data_snr = []
+sensor_id = []
+sensor_sensor = []
+temp_sensor_airtime = []
+sensor_bw = []
+sensor_sf = []
 
 with open(PATH, 'r') as file:
     raw_data = json.load(file)
 
 for i in range(len(raw_data) - 1):
     if (raw_data[i]["name"] == "as.up.data.forward"):
+        sensor_id.append(raw_data[i]["data"]["uplink_message"]["f_cnt"])
+        sensor_sensor.append(raw_data[i]["data"]["end_device_ids"]["device_id"])
+        temp_sensor_airtime.append(raw_data[i]["data"]["uplink_message"]["consumed_airtime"])
+        sensor_bw.append(raw_data[i]["data"]["uplink_message"]["settings"]["data_rate"]["lora"]["bandwidth"])
+        sensor_sf.append(raw_data[i]["data"]["uplink_message"]["settings"]["data_rate"]["lora"]["spreading_factor"])
         for j in range(len(raw_data[i]["data"]["uplink_message"]["rx_metadata"])):
-            id.append(raw_data[i]["data"]["uplink_message"]["f_cnt"])
-            sensor.append(raw_data[i]["data"]["end_device_ids"]["device_id"])
-            gateway.append(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["gateway_ids"]["gateway_id"])
-            rssi.append(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["rssi"])
-            snr.append(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["snr"])
+            data_id.append(raw_data[i]["data"]["uplink_message"]["f_cnt"])
+            data_gateway.append(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["gateway_ids"]["gateway_id"])
+            data_rssi.append(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["rssi"])
+            data_snr.append(raw_data[i]["data"]["uplink_message"]["rx_metadata"][j]["snr"])
+sensor_airtime = [float(t.replace('s', '')) for t in temp_sensor_airtime]
 
-DATA = pd.DataFrame({'GATEWAY': gateway,
-                    'RSSI': rssi,
-                    'SNR': snr},
-                    index=[id, sensor])
+SENSOR = pd.DataFrame({'SENSOR': sensor_sensor,
+                       'AIRTIME': sensor_airtime,
+                       'BW': sensor_bw,
+                       'SP': sensor_sf},
+                       index=[sensor_id])
+
+
+DATA = pd.DataFrame({'RSSI': data_rssi,
+                    'SNR': data_snr},
+                    index=[data_id, data_gateway])
 
 print(DATA)
+print(SENSOR)
